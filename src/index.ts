5e12940d68e248c419d93da713185555ccb976d2
winton <mail@wintoni.us>
2019-11-11 13:47:30 -0500

    Add access modifiers, alphabetize

--- src/index.ts
+++ src/index.ts
@@ -15,23 +15,92 @@ export class Router {
     }
   }
 
+  add(route: string, handler: Function): void {
+    const pieces = route.split("/")
+    let rules = this.routes
+
+    for (
+      let i = +(route[0] === "/");
+      i < pieces.length;
+      ++i
+    ) {
+      const piece = pieces[i]
+
+      let name: string
+
+      if (piece[0] === ":") {
+        name = ":"
+      } else if (piece[0] === "*") {
+        name = "*"
+      } else {
+        name = piece.toLowerCase()
+      }
+
+      rules = rules[name] || (rules[name] = {})
+      ;(name == ":" || name == "*") &&
+        (rules["~"] = piece.slice(1))
+    }
+
+    rules["@"] = handler
+  }
+
+  route(url: string, arg?: any): any {
+    const result = this.lookup(url)
+
+    if (result.cb) {
+      return result.cb(result.params, arg, url)
+    } else {
+      return this.route("/404")
+    }
+  }
+
   reset(): void {
     this.routes = {}
   }
 
-  noop(s: string): string {
+  private lookup(url: string): InternalRoute {
+    const querySplit = this.sanitize(url).split("?")
+    const esc = ~url.indexOf("%") ? this.decode : this.noop
+
+    return this.processQuery(
+      querySplit[1],
+      this.recurseUrl(
+        querySplit[0].split("/"),
+        esc,
+        0,
+        this.routes,
+        []
+      ) || {},
+      esc
+    )
+  }
+
+  private processQuery(
+    url: string,
+    ctx: InternalRoute,
+    esc: Function
+  ): InternalRoute {
+    if (url && ctx.cb) {
+      const hash = url.indexOf("#"),
+        query = (hash < 0 ? url : url.slice(0, hash)).split(
+          "&"
+        )
+
+      for (let i = 0; i < query.length; ++i) {
+        const nameValue = query[i].split("=")
+
+        ctx.params[nameValue[0]] = esc(nameValue[1])
+      }
+    }
+
+    return ctx
+  }
+
+  private noop(s: string): string {
     return s
   }
 
-  sanitize(url): string {
-    ~url.indexOf("/?") && (url = url.replace("/?", "?"))
-    url[0] == "/" && (url = url.slice(1))
-    url[url.length - 1] == "/" && (url = url.slice(0, -1))
-
-    return url
-  }
-
-  recurseUrl(
+  private recurseUrl(
     pieces: string[],
     esc: Function,
     i: number,
@@ -88,7 +157,7 @@ export class Router {
     )
   }
 
-  recurseNamedUrl(
+  private recurseNamedUrl(
     pieces: string[],
     esc: Function,
     i: number,
@@ -104,81 +173,12 @@ export class Router {
     return this.recurseUrl(pieces, esc, i, subRules, params)
   }
 
-  processQuery(
-    url: string,
-    ctx: InternalRoute,
-    esc: Function
-  ): InternalRoute {
-    if (url && ctx.cb) {
-      const hash = url.indexOf("#"),
-        query = (hash < 0 ? url : url.slice(0, hash)).split(
-          "&"
-        )
+  private sanitize(url): string {
+    ~url.indexOf("/?") && (url = url.replace("/?", "?"))
+    url[0] == "/" && (url = url.slice(1))
+    url[url.length - 1] == "/" && (url = url.slice(0, -1))
 
-      for (let i = 0; i < query.length; ++i) {
-        const nameValue = query[i].split("=")
-
-        ctx.params[nameValue[0]] = esc(nameValue[1])
-      }
-    }
-
-    return ctx
-  }
-
-  lookup(url: string): InternalRoute {
-    const querySplit = this.sanitize(url).split("?")
-    const esc = ~url.indexOf("%") ? this.decode : this.noop
-
-    return this.processQuery(
-      querySplit[1],
-      this.recurseUrl(
-        querySplit[0].split("/"),
-        esc,
-        0,
-        this.routes,
-        []
-      ) || {},
-      esc
-    )
-  }
-
-  add(route: string, handler: Function): void {
-    const pieces = route.split("/")
-    let rules = this.routes
-
-    for (
-      let i = +(route[0] === "/");
-      i < pieces.length;
-      ++i
-    ) {
-      const piece = pieces[i]
-
-      let name: string
-
-      if (piece[0] === ":") {
-        name = ":"
-      } else if (piece[0] === "*") {
-        name = "*"
-      } else {
-        name = piece.toLowerCase()
-      }
-
-      rules = rules[name] || (rules[name] = {})
-      ;(name == ":" || name == "*") &&
-        (rules["~"] = piece.slice(1))
-    }
-
-    rules["@"] = handler
-  }
-
-  route(url: string, arg?: any): any {
-    const result = this.lookup(url)
-
-    if (result.cb) {
-      return result.cb(result.params, arg, url)
-    } else {
-      return this.route("/404")
-    }
+    return url
   }
 }
 
